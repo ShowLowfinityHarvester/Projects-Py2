217b59d9dacc3fc502faa6a3325c42dd228108a8
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-07 09:00:21 -0700

    Added some stuff

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,8 +1,11 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-def race(Status, CarType):
-    print("You completed all laps and finished in " +Status+ " place in your " + CarType + ".")
-    
-race("1st", "1996 Chevy Impala ZLT V8")
-race("3rd", "2001 Mini Cooper S")
-race("4th", "2002 Ford Focus RS")
+mountains = {
+    "mountain1": "Sandia",
+    "mountain2": "Manzano",
+    "mountain3": "Sangre De Cristo",
+    "mountain4": "Rocky"
+
+}
+
+print(mountains)
