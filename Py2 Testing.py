5b005c01ac14e402f48c2a1a4f122223190980f1
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-08 09:44:26 -0700

    Update Py2 Testing.py

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,7 +1,12 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
 shape = input("What shape? -> ")
-height = int(input("How tall is the shape? -> "))
+while true:
+    try:
+            height = int(input("How tall is the shape? -> "))
+            break
+    except ValueError:
+          print("Number. ")
 
 shapes = {
 
