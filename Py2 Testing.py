69686e689b3dddb740e23a7b5477b487c2f5e409
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-08 10:14:38 -0700

    Update Py2 Testing.py

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,15 +1,10 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-goals = { "piano" : 5, "run" : 3, "paint" : 2, "serve" : 7, "homework" : 7}
-
-print("The following are your goals to complete: ")
-print(goals)
-goal = input("goal to remove? -> ")
-
-
-
-
-goals.pop(goal)
-
-
-print(goals)
+dinos = {}
+dino = input("What dino -> ")
+while True:
+    try:
+            dinoheight = int(input("Height? -> "))
+            break
+    except ValueError:
+          print("Please enter in a number.")
