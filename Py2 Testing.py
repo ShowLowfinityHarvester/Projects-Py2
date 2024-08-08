bc1b2cae1557cd07700c46fac028ae65493c5f7a
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-08 09:00:20 -0700

    Update Py2 Testing.py

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,11 +1,13 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-mountains = {
-    "mountain1": "Sandia",
-    "mountain2": "Manzano",
-    "mountain3": "Sangre De Cristo",
-    "mountain4": "Rocky"
+choice1 = int(input("What is the first number?"))
+choice2 = int(input("What is the second number?"))
 
-}
+def my_function(first, second):
+	if 0 < 2 :
+		print()
+	else:
+		print()
+		
 
-print(mountains)
+my_function(choice1,choice2)
