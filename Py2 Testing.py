b76439d3429a846a738cc4a3e1c10fc8aa7b31c1
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-08 09:30:58 -0700

    Update Py2 Testing.py

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,13 +1,8 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-choice1 = int(input("What is the first number?"))
-choice2 = int(input("What is the second number?"))
+friends = { "Shane" : 10, "Samantha" : 9, "Shiloh" : 12, "Sean" : 11 }
 
-def my_function(first, second):
-	if 0 < 2 :
-		print()
-	else:
-		print()
-		
-
-my_function(choice1,choice2)
+friends["Sebastian"] = 8
+print(friends)
+friends.pop("Shiloh")
+print(friends)
