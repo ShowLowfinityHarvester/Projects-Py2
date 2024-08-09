178f99d9b69dd9ee575f6a2fd71a3cb9ae444010
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-09 08:50:33 -0700

    Update Py2 Testing.py

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,8 +1,13 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-dictionary = { 1: "bicycle", 2: "soccer balls", 3: "piano books", }
+work = {
 
-dictionary[4] = input("What do you have 4 of? -> ")
-dictionary[5] = input("What do you have 5 of? -> ")
-dictionary[6] = input("What do you have 6 of?-> ")
-print(dictionary)
+	"Monday": 3, "Tuesday": 4, "Wednesday": 2, "Thursday": 1, "Friday": 1, 
+}
+work["Saturday"] = 0
+
+work.pop("Wednesday")
+print(len(work))
+
+if "Friday" in work:
+    print(work)
