3c585b990f202d5a44872cbb4e3b04a263f02bfe
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-09 08:42:45 -0700

    Update Py2 Testing.py

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,10 +1,8 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-coins = { "pennies" : 1, "nickels" : 2, "dimes" : 3, "quarters" : 4,}
+dictionary = { 1: "bicycle", 2: "soccer balls", 3: "piano books", }
 
-coins["silver dollar"] = 5
-
-coins.pop("pennies")
-
-print(coins)
-print(len(coins))
+dictionary[4] = input("What do you have 4 of? -> ")
+dictionary[5] = input("What do you have 5 of? -> ")
+dictionary[6] = input("What do you have 6 of?-> ")
+print(dictionary)
