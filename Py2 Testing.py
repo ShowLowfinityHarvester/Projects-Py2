9e78be0fa8505defd0f3f091efbdd6fb31415e0e
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-09 10:06:12 -0700

    Update Py2 Testing.py

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,5 +1,24 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-measurement = {"length": 10, "width": 5, "depth": 3}
-for x in measurement.values():
-    print(x)
+while True:
+    try: 
+        value1 = int(input("How many people is Amanda bringnig? -> "))
+        break
+    except ValueError:
+        print("Please enter in a number, not a name!")
+while True:
+    try: 
+        value2 = int(input("How many people is Jane bringnig? -> "))
+        break
+    except ValueError:
+        print("Please enter in a number, noT a name!")
+
+
+group = { "Fred" : 12, "Jackson" : 15, "Sophie" : 20, "Amanda" : value1, "Jane" : value2, }
+
+total = 0
+
+for x in group.values():
+	total += x
+	
+print(x)
