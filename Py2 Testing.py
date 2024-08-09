e7a932caf3363664957d96661656f6772963c48d
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-09 09:05:14 -0700

    Update Py2 Testing.py

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,13 +1,5 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-work = {
-
-	"Monday": 3, "Tuesday": 4, "Wednesday": 2, "Thursday": 1, "Friday": 1, 
-}
-work["Saturday"] = 0
-
-work.pop("Wednesday")
-print(len(work))
-
-if "Friday" in work:
-    print(work)
+measurement = {"length": 10, "width": 5, "depth": 3}
+for x in measurement.values():
+    print(x)
