88e5f91f5e734a30f2e0e73b45606e71daa15f11
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-12 09:28:51 -0700

    Stuffs

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,26 +1,9 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-while True:
-    try: 
-        value1 = int(input("pick a number -> "))
-        break
-    except ValueError:
-        print("Please enter in a number, not a name!")
-while True:
-    try: 
-        value2 = int(input("Pick another number -> "))
-        break
-    except ValueError:
-        print("Please enter in a number, noT a name!")
+ride = { "Jane" : 99, "Andy Candy" : 135, "Brodi" : 92, "Jolene" : 121, "Kyrene" : 102 }
 
-
-group = { 3 : 10, 5 : 3, 10 : 6, 4 : 30, value1 : value2 }
-
-
-
-total = 0
-for x,y in group.items():
-	total += x * y
-
-print(total)
-
+for x in ride.values():
+    if x >= 100:
+        print("This person is tall enough to ride.")
+else:
+    print("This person is too short to ride.")
--- SaveState.py
+++ SaveState.py
@@ -276,3 +276,15 @@ print(len(coins))
 measurement = {"length": 10, "width": 5, "depth": 3}
 for x in measurement.values():
     print(x)
+
+# Loops continued
+
+# These are school projects! Github page may be all cluttered until 2025 or 2026.
+
+ride = { "Jane" : 99, "Andy Candy" : 135, "Brodi" : 92, "Jolene" : 121, "Kyrene" : 102 }
+
+for x in ride.values():
+    if x >= 100:
+        print("This person is tall enough to ride.")
+else:
+    print("This person is too short to ride.")
