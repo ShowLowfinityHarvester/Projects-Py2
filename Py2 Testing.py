55c09f71490ae509f6c43714e4e3153467e557fc
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-13 08:05:48 -0700

    Update Py2 Testing.py

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,8 +1,16 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-ride = { "Jane" : 99, "Andy Candy" : 135, "Brodi" : 92, "Jolene" : 121, "Kyrene" : 102 }
+first = int(input("First Number -> "))
+second = int(input("Second Number -> "))
+third = int(input("Third Number -> "))
+fourth = int(input("Fourth Number -> "))
+fifth = int(input("Fith Number -> "))
 
-for x in ride.values():
+dictionary = { 7: first, 3: second, 4: third, 8: fourth, 9: fifth }
+
+my_list = [int(n) for n in input().split()]
+
+for x in dictionary.values():
     if x >= 100:
         print("This person is tall enough to ride.")
 else:
