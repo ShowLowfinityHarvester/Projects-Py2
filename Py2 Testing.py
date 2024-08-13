11531964d3f6b4f4417f739621dda1bfcea635a9
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-13 08:40:46 -0700

    Update Py2 Testing.py

--- Py2 Testing.py
+++ Py2 Testing.py
@@ -1,17 +1,19 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-first = int(input("First Number -> "))
-second = int(input("Second Number -> "))
-third = int(input("Third Number -> "))
-fourth = int(input("Fourth Number -> "))
-fifth = int(input("Fith Number -> "))
+dictionary = {
+  7: "first",
+  3: "second",
+  4: "third",
+  8: "fourth",
+  9: "fifth",
+}
 
-dictionary = { 7: first, 3: second, 4: third, 8: fourth, 9: fifth }
+my_list = [int(n) for n in input("Enter in a number with spaces -> ").split()]
 
-my_list = [int(n) for n in input().split()]
+for x in my_list:
 
-for x in dictionary.values():
-    if x >= 100:
-        print("This person is tall enough to ride.")
-else:
-    print("This person is too short to ride.")
+	if x in dictionary:
+		print("Yes")
+		
+	else:
+		print("No")
