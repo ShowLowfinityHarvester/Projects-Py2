4fa3756fb7aa58f40c5f495f1e3b635ece47ff4f
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-15 09:48:42 -0700

    Create Factorials.py

---
+++ Factorials.py
@@ -0,0 +1,11 @@
+number = int(input("What number do you want to check? -> "))
+
+mult_total = 1
+sum_total = 0
+
+for x in range(1, number+1):
+    sum_total = number * mult_total
+
+print((range(x)))
+
+# incomplete and buggy
