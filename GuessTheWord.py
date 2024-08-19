e1b7859c9a051f1d1c72d85d7d3f894b48dc818a
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-19 08:52:15 -0700

    Create GuessTheWord.py

---
+++ GuessTheWord.py
@@ -0,0 +1,5 @@
+# These are school projects! Github page may be all cluttered until 2025 or 2026.
+
+NMcityNames = ["Albuquerque", "Belen", "Curry", "LosAlamos", "Los Ranchos", "RioRancho", "Cochiti", "Ruidoso", "Quay", "Hobbs", "Carlsbad"]
+PickANum = int(input("Choose a number betweem 0-10 -> "))
+
