4d3670ff7f3fd98f50454b19b551db4c1781c478
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-19 09:40:21 -0700

    Update GuessTheWord.py

--- GuessTheWord.py
+++ GuessTheWord.py
@@ -1,5 +1,21 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-NMcityNames = ["Albuquerque", "Belen", "Curry", "LosAlamos", "Los Ranchos", "RioRancho", "Cochiti", "Ruidoso", "Quay", "Hobbs", "Carlsbad"]
-PickANum = int(input("Choose a number betweem 0-10 -> "))
+NMcityNames = ["Albuquerque", "Belen", "Curry", "LosAlamos", "LosRanchos", "RioRancho", "Cochiti", "Ruidoso", "Quay", "Hobbs", "Carlsbad"]
+PickANum = int(input("Choose a number between 0-10: -> "))
 
+InpCity = NMcityNames[PickANum]
+letters = list(InpCity)
+turns = 12
+
+while turns != 0:
+    myletter = (input("Input a letter: -> "))
+    if myletter in letters:
+      print("Letter is in word") 
+      letters.remove(myletter) 
+    else:
+      print("-")
+      turns -= 1
+    
+if turns == 0:
+    print("Game Over")
+    print(letters)
