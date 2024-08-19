6d7ff4abd66e8ea90e0d75e2680d6b7023dc4941
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-19 08:52:22 -0700

    Update Factorials.py

--- Factorials.py
+++ Factorials.py
@@ -1,3 +1,12 @@
-NMcityNames = ["Albuquerque", "Belen", "Curry", "LosAlamos", "Los Ranchos", "RioRancho", "Cochiti", "Ruidoso", "Quay", "Hobbs", "Carlsbad"]
-PickANum = int(input("Choose a number betweem 0-10 -> "))
+# These are school projects! Github page may be all cluttered until 2025 or 2026.
 
+number = int(input("What number do you want to check? -> "))
+
+mult_total = 1
+sum_total = 0
+
+for x in range(1, number+1):
+   mult_total *= x
+   sum_total += mult_total
+
+print(sum_total)
