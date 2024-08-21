7ea0b8b8a1239a20d8a9ee20ed8533836ea30c75
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-21 09:21:26 -0700

    Update ContactList.py

--- ContactList.py
+++ ContactList.py
@@ -1,6 +1,10 @@
+# These are school projects! Github page may be all cluttered until 2025 or 2026.
+
 answer = "y"
 listOfContacts = []
 while answer == "y":
     name = input("Add a contact? -> ")
-    listOfContacts.sort(name)
+    listOfContacts.append(name)
     answer = input("Add more contacts? (y/n) -> ")
+listOfContacts.sort()
+print(listOfContacts)
